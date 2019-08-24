148f2d05e395c7e6b7d262a6287bd657cb5c26bc
pauldev1128 <pauldev118@gmail.com>
2019-08-24 12:27:34 -0700

    successfully connected to my db

--- database/index.js
+++ database/index.js
@@ -1,5 +1,18 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost:3003/db')
+mongoose.connect('mongodb://localhost:27017/products', { useNewUrlParser: true })
 .then(() => console.log('db connected'))
 .catch((err) => console.log('db not connected'))
+
+const Products = new Schema({
+  property: String,
+  type: String,
+  title: [String],
+  price: [String],
+  img: [[[String]]]
+})
+
+const Lalalime = mongoose.model('LalaLime', Products )
+
+module.exports = Lalalime;
