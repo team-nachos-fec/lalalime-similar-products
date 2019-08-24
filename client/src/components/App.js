39ef80c6665c3b5dd7af3019a4cdb48c71be3969
pauldev1128 <pauldev118@gmail.com>
2019-08-24 16:03:31 -0700

    created my stateful app components

---
+++ client/src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import List from './List.js'
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        this is app
+      </div>
+    )
+  }
+}
+
+export default App;
