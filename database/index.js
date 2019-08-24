1963bfe1dc41485a14e4f661da360ada2c833b74
pauldev1128 <pauldev118@gmail.com>
2019-08-24 11:30:39 -0700

    created a connection for my db

--- database/index.js
+++ database/index.js
@@ -1,14 +1,5 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-
-const lululemon = new Schema({
-  property: String,
-  type: String,
-  img: [String],
-  color: [String],
-  title: String,
-  price: String
-})
-
-const category = ['Shorts', 'Skirts', 'Pants', 'Dresses', 'Tanks', 'Sweaters', 'Jackets', 'Sweatshirts','Short Sleeves', 'Long Sleeves'];
+mongoose.connect('mongodb://localhost:3003/db')
+.then(() => console.log('db connected'))
+.catch((err) => console.log('db not connected'))
